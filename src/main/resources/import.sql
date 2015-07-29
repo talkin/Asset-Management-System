ec696cfc263662d03b9a7ac1b1b47e1249a37f88
talkin <taojing1106@163.com>
2015-07-29 11:14:13 +0800

    [Jing] Add import.sql for local.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into Asset(asset_type, name, owner, status, order_date) values ('MPR', 'Laptop', 'Jing', 'Assigned', '2014-06-23')
+insert into Asset(asset_type, name, owner, status, order_date) values ('MPR', 'Laptop', 'Ying', 'Assigned', '2014-08-23')
